c8223f50b9ef3b2a2c660c81e9abaef3dcdb18a6
imonweb <contact@imonweb.co.uk>
2021-05-20 23:01:16 +0100

    initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
+]
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -17,7 +24,18 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader","css-loader"]
+        use: ["style-loader","css-loader", {
+          loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: postCSSPlugins
+              }
+            }
+        }]
+      },
+      {
+        test: /\.(jpg|jpeg|svg|png|gif)$/i,
+        type: 'asset/resource',
       }
     ]
   }
